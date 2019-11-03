4f2148bc0c83923d2de0460bde95beb65a734d21
Bruno Ramalho <bramalho@divante.pl>
2019-11-03 20:04:30 +0100

    api

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/bramalho/go-graphql-postgresql/gql"
+	"github.com/bramalho/go-graphql-postgresql/postgres"
+	"github.com/bramalho/go-graphql-postgresql/server"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+	"github.com/graphql-go/graphql"
+)
 
 func main() {
-	fmt.Println("It works!")
+	router, db := initializeAPI()
+	defer db.Close()
+
+	log.Fatal(http.ListenAndServe(":8088", router))
+}
+
+func initializeAPI() (*chi.Mux, *postgres.Db) {
+	router := chi.NewRouter()
+
+	db, err := postgres.New(
+		postgres.ConnString("localhost", 5432, "user", "password", "go_graphql_postgresql"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rootQuery := gql.NewRoot(db)
+	sc, err := graphql.NewSchema(
+		graphql.SchemaConfig{Query: rootQuery.Query},
+	)
+	if err != nil {
+		fmt.Println("Error creating schema: ", err)
+	}
+
+	s := server.Server{
+		GqlSchema: &sc,
+	}
+
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON),
+		middleware.Logger,
+		middleware.DefaultCompress,
+		middleware.StripSlashes,
+		middleware.Recoverer,
+	)
+
+	router.Get("/graphql", s.GraphQL())
+
+	return router, db
 }
