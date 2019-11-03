27c6932473f4a9ee510792700fcebd448e6c2646
Bruno Ramalho <bramalho@divante.pl>
2019-11-03 20:04:13 +0100

    graph

---
+++ gql/queries.go
@@ -0,0 +1,34 @@
+package gql
+
+import (
+	"github.com/bramalho/go-graphql-postgresql/postgres"
+	"github.com/graphql-go/graphql"
+)
+
+type Root struct {
+	Query *graphql.Object
+}
+
+func NewRoot(db *postgres.Db) *Root {
+	resolver := Resolver{db: db}
+
+	root := Root{
+		Query: graphql.NewObject(
+			graphql.ObjectConfig{
+				Name: "Query",
+				Fields: graphql.Fields{
+					"users": &graphql.Field{
+						Type: graphql.NewList(User),
+						Args: graphql.FieldConfigArgument{
+							"name": &graphql.ArgumentConfig{
+								Type: graphql.String,
+							},
+						},
+						Resolve: resolver.UserResolver,
+					},
+				},
+			},
+		),
+	}
+	return &root
+}
